ebe5464afb0083f728111e47e7aaf7e80bedd51d
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-03-20 22:59:36 -0500

    commit tree 711e215049596684b2884b8dd539f838a2bd1cda

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const driver = {}
+
+function updateDriverWithKeyAndValue(driver, key, value){
+
+    const newDriver = Object.assign({}, driver, { [key]: `${value}`})
+    return newDriver
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+    // const updatedDriver = Object.assign(driver, {[key]: value})
+    // return updatedDriver
+    driver[key] = value
+    return driver
+}
+
+function deleteFromDriverByKey(driver, key){
+    const newDriver = {...driver}
+    delete newDriver[key]
+    return newDriver
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key){
+    delete driver[key]
+    return driver
+}
